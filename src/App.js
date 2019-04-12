cbdff47fce53cfda4bf14f0b9e076b1e5b92b20d
Felipe Maciel <felipemrvieira@gmail.com>
2019-04-12 13:25:52 -0300

    Adiciona dependencias e exibe componente header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Header title="Agência Alagoas" name="Governo do Estado de Alagoas" />
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +22,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </div>
       </div>
     );
   }
